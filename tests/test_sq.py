5e2e5c3d752f24fb67c435e13f35a339f9a79128
Junaid-e <hameed.as@nulondon.ac.uk>
2026-04-25 22:34:54 +0100

    added test root of

--- tests/test_sq.py
+++ tests/test_sq.py
@@ -1,9 +1,14 @@
 import unittest
+from roots import sq_num
 
 #I have called the class "SmokeTest. It is using unittest.TestCase from the unittest import.
 class SmokeTest(unittest.TestCase):
     def test2_and_2(self):
         self.assertEqual(2+2,4)
+
+class TestSquare(unittest.TestCase):
+    def test_root_of_0(self):
+        self.assertEqual(sq_num(0),0)
 
 if __name__ == "__main__":
     unittest.main()
